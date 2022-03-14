23824ad1afaaf572c1bf965fe6361897e793f08c
david <david.l.xie@gmail.com>
2022-03-13 23:36:05 -0700

    Updated main.py, menu.py, and navbar.html. Added __init__.py, base.html, index.html, and swap.py

--- src/menu.py
+++ src/menu.py
@@ -1,111 +1,41 @@
-# menuy.py - function style menu
-# Imports typically listed at top
-# each import enables us to use logic that has been abstracted to other files and folders
-# import loopy
-# import mathpy
-# from mody import advy
-# # abstracted files in a folder (aka module)
-# from mody import questy
-# from wipy import funcy
-# from wipy import prefuncy
-# import patterns
+# menuy.py - list style menu
+import  random
 
+main_menu = {
+    "Appetizer": ["Garlic Bread", "Onion Rings", "Chips"],
+    "Beverage": ["Lemonade", "Water", "Coca Cola"],
+    "Main Dish": ["Pizza", "Pasta", "Burgers"],
+    "Dessert": ["Ice Cream", "Cake", "Confections"]
+}
 
-# Main list of [Prompts, Actions]
-# Two styles are supported to execute abstracted logic
-# 1. file names will be run by exec(open("filename.py").read())
-# 2. function references will be executed directly file.function()
-main_menu = [
-    ["Beverage", "stringy.py"],
-    ["Main Dish", "listy.py"],
-    ["Dessert", "loopy"],
-]
-
-# Submenu list of [Prompt, Action]
+# Submenu
 # Works similarly to main_menu
-sub_menu = [
-    ["Factors", 2],
-    ["GCD", 3],
-    ["LCM", 4],
-    ["Primes", 5],
-]
+sub_menu = {
+    "Pizza": ["Cheese", "Pepperoni", "Combo"],
+    "Pasta": ["Lasagna", "Fettuccine", "Macaroni and Cheese"],
+    "Burgers": ["Cheeseburger", "Hamburger", "Double Burger"]
+}
 
-patterns_sub_menu = [
-    ["Patterns", "patterns.py"],
-    ["PreFuncy", "wipy/prefuncy.py"],
-    ["Funcy", "funcy.py"],
-]
 
 # Menu banner is typically defined by menu owner
 border = "=" * 25
 banner = f"\n{border}\nPlease Select An Option\n{border}"
 
 # def menu
-# using main_menu list:
-# 1. main menu and submenu reference are created [Prompts, Actions]
-# 2. menu_list is sent as parameter to menuy.menu function that has logic for menu control
 def menu():
-    title = "Food Menu" + banner
-    menu_list = main_menu.copy()
-    menu_list.append(["Math", submenu])
-    menu_list.append(["Patterns", patterns_submenu])
-    buildMenu(title, menu_list)
-
-# def submenu
-# using sub menu list above:
-# sub_menu works similarly to menu()
-def submenu():
-    title = "Function Submenu" + banner
-    buildMenu(title, sub_menu)
-def patterns_submenu():
-    title = "Function Submenu" + banner
-    buildMenu(title, patterns_sub_menu)
-
-def buildMenu(banner, options):
-    # header for menu
-    print(banner)
-    # build a dictionary from options
-    prompts = {0: ["Exit", None]}
-    for op in options:
-        index = len(prompts)
-        prompts[index] = op
-
-    # print menu or dictionary
-    for key, value in prompts.items():
-        print(key, '->', value[0])
-
-    # get user choice
-    choice = input("Type your choice> ")
-
-    # validate choice and run
-    # execute selection
-    # convert to number
-    try:
-        choice = int(choice)
-        if choice == 0:
-            # stop
-            return
-        try:
-            # try as function
-            action = prompts.get(choice)[1]
-            action()
-        except TypeError:
-            try:  # try as playground style
-                exec(open(action).read())
-            except FileNotFoundError:
-                print(f"File not found!: {action}")
-            # end function try
-        # end prompts try
-    except ValueError:
-        # not a number error
-        print(f"Not a number: {choice}")
-    except UnboundLocalError:
-        # traps all other errors
-        print(f"Invalid choice: {choice}")
-    # end validation try
-
-    buildMenu(banner, options)  # recursion, start menu over again
-
+    order =[]
+    title = "David's Restaurant Menu" + banner
+    print(title)
+    counter = 1
+    for i in main_menu.keys():
+        print(counter, i)
+        counter = counter + 1
+        random.shuffle(main_menu.get(i))
+        for j in main_menu.get(i):
+            print("\t", j)
+        choice = input()
+        order.append(i+":"+choice)
+    print("Your order is: ", order)
 
 if __name__ == "__main__":
     menu()
