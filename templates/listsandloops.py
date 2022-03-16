88346b43b1be376c9c3031f1e973439b1ff5eec1
David-Xie123 <89278191+David-Xie123@users.noreply.github.com>
2022-03-16 11:54:40 -0700

    Update listsandloops.py

--- templates/listsandloops.py
+++ templates/listsandloops.py
@@ -29,8 +29,21 @@ def print_data(n):
 
 # Hack 2: InfoDB loops. Print values from the lists using three different ways: for, while, recursion
 ## hack 2a: def for_loop()
+def for_loop():
+    for n in range(len(InfoDb)):
+        print_data(n)
 ## hack 2b: def while_loop(0)
+def while_loop(n):
+    while n < len(InfoDb):
+        print_data(n)
+        n += 1
+    return
 ## hack 2c : def recursive_loop(0)
+def recursive_loop(n):
+    if n < len(InfoDb):
+        print_data(n)
+        recursive_loop(n + 1)
+    return # exit condition
 
 def tester1():
     print("For loop")
@@ -39,25 +52,6 @@ def tester1():
     while_loop(0)  # requires initial index to start while
     print("Recursive loop")
     recursive_loop(0)  # requires initial index to start recursion
-    
-# for loop iterates on length of InfoDb
-def for_loop():
-    for n in range(len(InfoDb)):
-        print_data(n)
-        
-# while loop contains an initial n and an index incrementing statement (n += 1)
-def while_loop(n):
-    while n < len(InfoDb):
-        print_data(n)
-        n += 1
-    return
-
-# recursion simulates loop incrementing on each call (n + 1) until exit condition is met
-def recursive_loop(n):
-    if n < len(InfoDb):
-        print_data(n)
-        recursive_loop(n + 1)
-    return # exit condition
 
 # Factorial of a number using recursion
 def recur_factorial(n):
@@ -76,3 +70,17 @@ def tester2():
         print("The factorial of", num, "is", recur_factorial(num))
 
 # Hack 3: Fibonacci.  Write a recursive program to create a fibonacci sequence including error handling for invalid input
+def recur_fibonacci(n):  
+   if n <= 1:  
+       return n  
+   else:  
+       return(recur_fibonacci(n-1) + recur_fibonacci(n-2))  
+# take input from the user  
+nterms = int(input("Enter a number for a Fibonacci sequence: "))  
+# check if the number of terms is valid  
+if nterms <= 0:  
+   print("The Fibonacci sequence for that number does not exist")  
+else:  
+   print("The Fibonacci sequence for that number is")  
+   for i in range(nterms):  
+       print(recur_fibo(i))  
